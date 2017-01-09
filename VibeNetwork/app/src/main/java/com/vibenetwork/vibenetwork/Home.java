1b2a74e14ad11daa14c5be02bc367ce2173ac563
Antoine Ménand <antoine.menand@telecom-paristech.fr>
2017-01-09 12:13:14 +0100

    upload du projet VibeNetwork

---
+++ VibeNetwork/app/src/main/java/com/vibenetwork/vibenetwork/Home.java
@@ -0,0 +1,37 @@
+package com.vibenetwork.vibenetwork;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Button signInButton=(Button) findViewById(R.id.signInButton);
+        Button signUpButton=(Button) findViewById(R.id.signUpButton);
+        Button adminButton=(Button) findViewById(R.id.adminButton);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                Intent signInIntent=new Intent(Home.this,Map.class);
+                startActivity(signInIntent);
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                Intent signUpIntent=new Intent(Home.this,SignUp.class);
+                startActivity(signUpIntent);
+            }
+        });
+
+
+    }
+}
---
+++ VibeNetwork/app/src/main/java/com/vibenetwork/vibenetwork/Suggestions.java
@@ -0,0 +1,13 @@
+package com.vibenetwork.vibenetwork;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Suggestions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suggestions);
+    }
+}
