2c2b0a6542ffb1aafbd4e8f204cc2eb97a58ad52
Alban GAUTHIER <alban.gauthier@telecom-paristech.fr>
2017-01-09 15:51:52 +0100

    ignored new files, added a class AlgorithmeCC for the connected component algorithm, created ImageBinaireIO class, added main method in Main class

--- Traitementimage/Java/src/AlgorithmeCC.java
+++ Traitementimage/Java/src/AlgorithmeCC.java
@@ -1,15 +1,9 @@
 import java.util.ArrayList;
 
-public class ImageBinaire {
 
-	private ArrayList<ArrayList<Integer>> imagebinaire ;
+public class AlgorithmeCC {	
 	
-	public ImageBinaire(ArrayList<ArrayList<Integer>> imagebinaire){
-		
-		this.imagebinaire = imagebinaire ;
-	}
-	
-	private int[][] main(ArrayList<ArrayList<Integer>> image){
+	private static int[][] firstPhase(ArrayList<ArrayList<Integer>> image){
 		
 		int[] predecessor = new int[30];
 		int h = image.size() ;
@@ -19,12 +13,11 @@ public class ImageBinaire {
 		for (int i=0; i<h ; i++){
 			for(int j=0; j<w ; j++){	//browse from top to bottom, from left to right
 				if (j>0) {	//if not on the upper edge
-					if (image.get(i).get(j-1) != 0){	//if upper neighbor already is in a connected component
-						imagecompo[i][j] = image.get(i).get(j-1); //go in the same component
+					if (imagecompo[i][j-1] != 0){	//if upper neighbor already is in a connected component
+						imagecompo[i][j] = imagecompo[i][j-1]; //go in the same component
 				if(i>0){ //if not on the left edge
 					if (image.get(i-1).get(j) != 0) //if left neighbor differs from upper neighbor
 						predecessor[image.get(i-1).get(j)] = image.get(i).get(j-1);
-					if vfgeszfes
 						}
 					}
 				}
--- Traitementimage/Java/src/Main.java
+++ Traitementimage/Java/src/Main.java
@@ -1,8 +1,33 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Main {
+  
+	public static void main(String[] argv) throws Exception {
 
-	public Main(){
-		
+    int width = 100;
+    int height = 100;
+
+    /**
+     * BufferedImage bimage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+     * bimage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+     */
+   
 	}
 	
+	public final void initFromTiffFile(File inputfile) {
+		
+		BufferedImage image ;
+		
+		try {
+			image = ImageIO.read(inputfile);
+			
+		} catch (IOException e) {
+			System.out.println("Error: " + e);
+		}
+	}
+
 }
